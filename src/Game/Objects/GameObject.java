26e646ce1692a85c43cdd6eb7163b830f8a7b809
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-01-08 19:29:09 +0300

    Update GameObject

--- src/Game/Objects/GameObject.java
+++ src/Game/Objects/GameObject.java
@@ -6,16 +6,23 @@ import java.awt.*;
 /**
  * The class describes a game object
  */
-public abstract class GameObject {
+public abstract class GameObject extends JComponent {
     protected int xCoordinate, yCoordinate;
     protected int sizeX, sizeY;
-    protected Color backgroundColor;
+    protected Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public abstract void paintComponent(Graphics g);
 
     public GameObject(int xCoordinate, int yCoordinate, int sizeX, int sizeY, Color color) {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
         this.sizeX = sizeX;
         this.sizeY = sizeY;
-        this.backgroundColor = color;
+        this.color = color;
     }
 }
