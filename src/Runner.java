a445acf76df6f14111dea3cbddbbfd743b4e6580
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-01-13 11:54:24 +0300

    Update class Runner

--- src/Runner.java
+++ src/Runner.java
@@ -3,6 +3,6 @@ import Game.Game;
 public class Runner {
     public static void main(String[] args) {
         Game game = new Game();
-        game.drawGameField();
+        game.startGame();
     }
 }
