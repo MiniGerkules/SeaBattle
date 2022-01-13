e82f0c9afa15d00b47a244ae331d5776151b17d3
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-01-13 11:54:14 +0300

    Update class Game

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,20 +6,24 @@ import java.util.List;
 import javax.swing.*;
 
 import Game.Objects.GameObject;
-import Game.Objects.GameObjectsContainer;
+import Game.Objects.GameField;
 
 /**
  * The main class of the game. Determines the main window and its rendering
  */
 public class Game {
-    public final int appSizeX, appSizeY;
-
     private final JFrame game = new JFrame(); // Основной экран приложения
+    private final Container mainContainer = game.getContentPane(); // Главный контейнер приложения
+    private final JPanel gameFields = new JPanel(); // Контейнер, в который будем складывать поля игроков
+
     private final List<GameObject> allGameObjects = new ArrayList<>();
-    private final GameObjectsContainer container;
+    private final GameField player1Field;
+    private final GameField player2Field;
 
     private Player player1;
     private Player player2;
+
+    public final int appSizeX, appSizeY;
 
     /**
      * Constructor of the Game class. Initializes the main frame of the game.
@@ -32,16 +36,21 @@ public class Game {
         int appBoundY = (dimension.height - appSizeY) / 2;
 
         game.setTitle("Sea Battle"); // Устанавливаем заголовок
-        game.setBounds(appBoundX, appBoundY, appSizeX, appSizeY); // Устанавливаем границы и размеры
+        game.setLocation(appBoundX, appBoundY); // Задаем расположение главного окна
         game.setResizable(false); // Задаем возможность изменения размера
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Задаем способ закрытия приложения
 
+        mainContainer.setLayout(new BorderLayout()); // Выбираем контейнер
+        mainContainer.setPreferredSize(new Dimension(appSizeX, appSizeY)); // Выставляем размеры без учета заголовка
+        mainContainer.add(gameFields, BorderLayout.CENTER);
+        gameFields.setLayout(new GridLayout(1, 2));
+        game.pack(); // Заставляем размеры быть применеными
+
         int bias = 10;
-        int contSizeX = appSizeX / 2 - bias*2;
-        int contSizeY = appSizeY - bias*2;
-        int contBoundX = (appSizeX - contSizeX) / 2;
-        int contBoundY = (appSizeY - contSizeY) / 2;
-        container = new GameObjectsContainer(contBoundX, contBoundY, contSizeX, contSizeY, new Color(64,224,208));
+        int gridSizeX = appSizeX/2 - bias*2;
+        int gridSizeY = appSizeY - bias*2;
+        player1Field = new GameField(10, bias, bias, gridSizeX, gridSizeY, new Color(64,224,208));
+        player2Field = new GameField(10, bias, bias, gridSizeX, gridSizeY, new Color(250, 128, 114));
 
         drawPreStartMenu();
     }
@@ -57,10 +66,11 @@ public class Game {
      * The method starts the game if all right
      */
     public void startGame() {
+        gameFields.add(player1Field);
+        gameFields.add(player2Field);
 
-    }
-
-    private void drawGameField() {
-        game.setVisible(true);
+//        gameFields.remove(player1Field);
+//        gameFields.remove(player2Field);
+//        gameFields.repaint();
     }
 }
