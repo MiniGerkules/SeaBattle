756b88d723b5d33c20ce7f789c272e7edd132ab1
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-02-02 16:39:33 +0300

    Add equals, hashcode methods

--- src/Game/Helpers/Index.java
+++ src/Game/Helpers/Index.java
@@ -1,5 +1,7 @@
 package Game.Helpers;
 
+import java.util.Objects;
+
 public class Index {
     private final int column;
     private final int row;
@@ -14,5 +16,18 @@ public class Index {
     public Index(int row, int column) {
         this.row = row;
         this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Index index = (Index) o;
+        return column == index.column && row == index.row;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column, row);
     }
 }
