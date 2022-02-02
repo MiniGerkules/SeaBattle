8d421dd08c6be124abad29b8eb0504654c744faa
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-02-02 15:51:51 +0300

    Rename indexX and indexY into row and column

--- src/Game/Objects/FieldPart.java
+++ src/Game/Objects/FieldPart.java
@@ -6,20 +6,19 @@ import java.awt.event.ActionListener;
 import java.util.Objects;
 
 public class FieldPart extends JButton {
-    private final int indexX;
-    private final int indexY;
+    private final int row;
+    private final int column;
 
-    public int getIndexX() {
-        return indexX;
+    public int getRow() {
+        return row;
+    }
+    public int getColumn() {
+        return column;
     }
 
-    public int getIndexY() {
-        return indexY;
-    }
-
-    public FieldPart(int indexX, int indexY, ActionListener listener) {
-        this.indexX = indexX;
-        this.indexY = indexY;
+    public FieldPart(int row, int column, ActionListener listener) {
+        this.row = row;
+        this.column = column;
 
         setBackground(Game.waterColor);
         addActionListener(listener);
@@ -38,13 +37,13 @@ public class FieldPart extends JButton {
         if (this == other)
             return true;
         else if (other instanceof FieldPart)
-            return ((FieldPart) other).indexX == indexX && ((FieldPart) other).indexY == indexY;
+            return ((FieldPart) other).row == row && ((FieldPart) other).column == column;
         else
             return false;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(indexX, indexY);
+        return Objects.hash(row, column);
     }
 }
